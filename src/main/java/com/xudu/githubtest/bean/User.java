b7852be3163ade1abec87de222ec533bc9e484fe
xudu <xudu_yinuo@163.com>
2024-10-15 20:13:35 +0800

    添加user

---
+++ src/main/java/com/xudu/githubtest/bean/User.java
@@ -0,0 +1,14 @@
+package com.xudu.githubtest.bean;
+
+/**
+ * @className: User
+ * @description: TODO
+ * @author: xudu
+ * @create: 2024-10-15
+ */
+
+public class User {
+
+    private String username;
+    private String password;
+}
