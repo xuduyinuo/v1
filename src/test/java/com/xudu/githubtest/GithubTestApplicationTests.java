f3870bd8b9f520b426947db27440d6df273b660c
xuduyinuo <xudu_yinuo@163.com>
2024-10-15 14:59:30 +0800

    初始化

---
+++ src/test/java/com/xudu/githubtest/GithubTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xudu.githubtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
